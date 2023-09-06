efd1d0f2f1f03c9e790e6ce5883fdbeff25f648b
enoch <enokkang@gmail.com>
2023-09-06 16:00:04 -0500

    adds conditional:jwt. checks if there is jwt data

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,16 @@
 import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { Logout } from "./Logout";
 
 export function Content() {
   return (
     <div>
       <Signup />
+      <p>&nbsp;</p>
+      <Login />
+      <p>&nbsp;</p>
+      <Logout />
+      <p>&nbsp;</p>
       <h1>Welcome to React!</h1>
     </div>
   );
