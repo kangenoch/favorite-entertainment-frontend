a5bfbc9acabb093aac42a70c63bf973d1dba9c28
enoch <enokkang@gmail.com>
2023-09-07 15:38:20 -0500

    adds filters to favoriteindex

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -14,14 +14,17 @@ export function FavoritesIndex(props) {
       </div>
       Search Filter:
       <input type="text" value={searchFilter} onChange={(e) => setSearchFilter(e.target.value)} list="favorites" />
-      {props.favorites.map((favorite) => (
-        <>
-          <div>
-            <li key={favorite.id}>Item_id: {favorite.item_id}</li>
-          </div>
-          <p>&nbsp;</p>
-        </>
-      ))}
+      {props.favorites
+        .filter((favorite) => favorite.user_id == localStorage.getItem("user_id"))
+        .map((favorite) => (
+          <>
+            <div>
+              <li key={favorite.id}>Item_id: {favorite.item_id}</li>
+              <button onClick={() => props.onShowItem(favorite)}>Show Item</button>
+            </div>
+            <p>&nbsp;</p>
+          </>
+        ))}
     </>
   );
 }
